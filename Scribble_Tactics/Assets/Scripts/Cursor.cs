bfb9fd9277b38751a4c9a67398a3533f43a23917
wlittle5 <wlornelittle@gmail.com>
2025-11-16 10:23:06 -0600

    Visual Assets and Camera Beginnings
    This addition is really just a crummy couple updates to the visuals and the start of me making a virtual camera to follow and in game cursor.

---
+++ Scribble_Tactics/Assets/Scripts/Cursor.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+    private float yMoveSpeed = 6f;
+    private float xMoveSpeed = 6f;
+
+    [SerializeField] private Transform cursor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
