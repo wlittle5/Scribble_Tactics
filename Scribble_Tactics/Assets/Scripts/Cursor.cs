f6975907d273b6124378ca0d0e7134feae6de357
wlittle5 <wlornelittle@gmail.com>
2025-11-16 20:59:55 -0600

    Added  cursor
    Added an object that the camera will follow and one day it will be a cursor and not a sphere.

--- Scribble_Tactics/Assets/Scripts/Cursor.cs
+++ Scribble_Tactics/Assets/Scripts/Cursor.cs
@@ -4,20 +4,35 @@ using UnityEngine;
 
 public class Cursor : MonoBehaviour
 {
-    private float yMoveSpeed = 6f;
-    private float xMoveSpeed = 6f;
-
-    [SerializeField] private Transform cursor;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float moveSpeed = 6f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 inputVector = new Vector2(0, 0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = +1;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1;
+        }
+
+        inputVector = inputVector.normalized;
+
+        transform.position += (Vector3)inputVector * moveSpeed * Time.deltaTime;
     }
 }
