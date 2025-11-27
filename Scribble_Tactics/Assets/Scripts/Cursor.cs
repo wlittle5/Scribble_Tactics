7fc2a6c5b733a8e6eedfdc81f9668f0d108fc27d
wlittle5 <wlornelittle@gmail.com>
2025-11-26 22:04:26 -0600

    Edge Scrolling
    Made several improvements in the camera system, namely edge scrolling.  Also made it where you can mess with the speed of the movement and have separate movement speeds for WASD movement and edge scrolling movement.

--- Scribble_Tactics/Assets/Scripts/Cursor.cs
+++ Scribble_Tactics/Assets/Scripts/Cursor.cs
@@ -1,40 +1,91 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class Cursor : MonoBehaviour
 {
-    private float moveSpeed = 6f;
+    [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
 
-    // Update is called once per frame
-    // Unless its not, then it just wont work. 
-    // also, you cant tell me what to do.
+    //Only for WASD movement speed
+    [SerializeField] private float moveSpeed = 6f;
+
+    //Edge scrolling variables
+    [SerializeField] private float edgeSpeed = 20f;
+    [SerializeField] private int edgeScrollSize = 20;
+    [SerializeField] private bool edgeScroll = true;
+
+    //Zoom variables
+    [SerializeField] private float followOffsetMin = 5f;
+    [SerializeField] private float followOffsetMax = 50f;
+    [SerializeField] private float zoomSpeed = 6f;
+
+    private Vector3 followOffset;
+
+    private void Awake()
+    {
+        followOffset = cinemachineVirtualCamera.GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset;
+    }
     void Update()
+    {
+        HandleCameraMovement();
+        HandleCameraEdgeMovement();
+        HandleCameraZoom();
+    }
+
+    private void HandleCameraMovement()
     {
         Vector2 inputVector = new Vector2(0, 0);
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
+        //WASD camera movement logic
+        if (Input.GetKey(KeyCode.W)) inputVector.y = +1;
+        if (Input.GetKey(KeyCode.A)) inputVector.x = -1;
+        if (Input.GetKey(KeyCode.S)) inputVector.y = -1;
+        if (Input.GetKey(KeyCode.D)) inputVector.x = +1;
 
         inputVector = inputVector.normalized;
 
         transform.position += (Vector3)inputVector * moveSpeed * Time.deltaTime;
+    }
+
+    private void HandleCameraEdgeMovement()
+    {
+        Vector2 edgeInputVector = new Vector2(0, 0);
+
+        //Edge Scrolling logic
+        if (Input.mousePosition.x < edgeScrollSize && edgeScroll) edgeInputVector.x = -1f;
+        if (Input.mousePosition.y < edgeScrollSize && edgeScroll) edgeInputVector.y = -1f;
+        if (Input.mousePosition.x > Screen.width - edgeScrollSize && edgeScroll) edgeInputVector.x = +1f;
+        if (Input.mousePosition.y > Screen.height - edgeScrollSize && edgeScroll) edgeInputVector.y = +1f;
+
+
+        edgeInputVector = edgeInputVector.normalized;
+        transform.position += (Vector3)edgeInputVector * edgeSpeed * Time.deltaTime;
+    }
+
+    private void HandleCameraZoom()
+    {
+        Vector3 zoomDir = followOffset.normalized;
+
+        float zoomAmount = 3f;
+
+        if (Input.mouseScrollDelta.y > 0)
+            followOffset += zoomDir * zoomAmount;
+        
+
+        if (Input.mouseScrollDelta.y < 0)
+            followOffset -= zoomDir * zoomAmount;
+        
+
+        /*if (followOffset.magnitude < followOffsetMin)        
+            followOffset = zoomDir * followOffsetMin;
+        
+        
+        if (followOffset.magnitude > followOffsetMax)        
+            followOffset = zoomDir * followOffsetMax;*/
+        
+
+        cinemachineVirtualCamera.GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset = Vector3.Lerp(cinemachineVirtualCamera.GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset, followOffset, Time.deltaTime);
+        
     }
 }
