0f7ea7a8e1fd7821688285a5cde9973c9425aea7
wlittle5 <wlornelittle@gmail.com>
2026-02-01 10:57:33 -0600

    Create CursorController.cs

---
+++ Scribble_Tactics/Assets/Scripts/CursorController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+    //Testing
+    [SerializeField] Texture2D cursor;
+    [SerializeField] Texture2D cursorClicked;
+
+    private CursorControls controls;
+    private void Awake()
+    {
+        controls = new CursorControls();
+        ChangeCursor(cursor);
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    private void Start()
+    {
+        controls.Mouse.Click.started += _ => StartedClick();
+        controls.Mouse.Click.performed += _ => EndClick();
+    }
+
+    private void StartedClick()
+    {
+        ChangeCursor(cursorClicked);
+    }
+
+    private void EndClick()
+    {
+        ChangeCursor(cursor);
+    }
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+    private void ChangeCursor(Texture2D cursorType)
+    {
+        Cursor.SetCursor(cursorType, Vector2.zero, CursorMode.Auto);
+    }
+}
