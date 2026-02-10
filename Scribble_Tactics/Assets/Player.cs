1bef3fe2c1fe8f550fefe8fd7fd9c43cf51b7435
wlittle5 <wlornelittle@gmail.com>
2026-02-09 21:28:25 -0600

    Player Cube
    Added a player cube that can do stuff when clicked.  Still need to figure out how to get the logic onto the parent object instead of the visual object AND have to figure how to truly disable the second object that is meant to represent the range of movement possible.

---
+++ Scribble_Tactics/Assets/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Windows;
+
+public class Player : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        Debug.Log("The button was pressed!");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
