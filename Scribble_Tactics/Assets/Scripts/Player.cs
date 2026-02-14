d9a88e18767e4045f984cca180dfb2c499dd483c
wlittle5 <wlornelittle@gmail.com>
2026-02-13 21:23:57 -0600

    Movement Working
    It works!!!!!  You can now select the player and then move them to the mouse point in world space provided it's on the paper.  Yay!!!!

--- Scribble_Tactics/Assets/Scripts/Player.cs
+++ Scribble_Tactics/Assets/Scripts/Player.cs
@@ -7,62 +7,64 @@ using UnityEngine.Windows;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private LayerMask collisionMask;
     [SerializeField] Camera myCamera;
-    private bool isSelected = false;
+    
+    public bool isSelected = false;
+    private int hitData;
+    private RaycastHit rayCastHit;
     Vector3 mousePos;
-    RaycastHit rayCastHit;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
-    {
-           
+    {      
         if (Input.GetMouseButtonUp(0))
         {
-            if (Physics.Raycast(myCamera.ScreenPointToRay(Input.mousePosition), out rayCastHit, Mathf.Infinity, collisionMask))
-            { 
-                if (!isSelected)
-                {
-                    isSelected = true;
-                    Debug.Log(isSelected);
-                }
-                
-                /*else if (isSelected == true)
-                {
-                    mousePos.z = transform.position.z;
-                    worldPos = myCamera.ScreenToWorldPoint(mousePos);
-                    transform.position = worldPos;
-                    isSelected = false;
-                }
-                */
-            }
-
-            else if (!Physics.Raycast(myCamera.ScreenPointToRay(Input.mousePosition), out rayCastHit, Mathf.Infinity, collisionMask))
-            {
-                if (!isSelected)
-                {
-                    //Do nothing
-                }
-
-                else if (isSelected)
-                {
-                    mousePos = myCamera.ScreenToWorldPoint(Input.mousePosition);
-                    mousePos.z = transform.position.z;
-                    transform.position = mousePos;
-                    Debug.Log(mousePos);
-                    isSelected = false;
-                }
-                
-            }
-
+            GetLayer();
+            if (MoveCheck())
+                MoveCharacter();
         }
         
+    }
+
+    private void GetLayer()
+    {
+        Ray myRay = myCamera.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(myRay, out rayCastHit);
+        hitData = rayCastHit.transform.gameObject.layer;
+    }
+
+    private bool MoveCheck()
+    {
+        Debug.Log(hitData);
+        if ((hitData == 6) && !isSelected)
+        {
+            isSelected = true;
+            return false;
+        }
+
+        if ((hitData == 8) && isSelected)
+        {
+            return true;
+        }
+
+        if ((hitData != 8) || (hitData != 6) && isSelected)
+        {
+            isSelected = false;
+            return false;
+        }
+
+        else
+        {
+            return false;
+        }
+    }
+
+    private void MoveCharacter()
+    {
+        mousePos = rayCastHit.point;
+        mousePos.z = transform.position.z;
+        transform.position = mousePos;
+        isSelected = false;
     }
 }
