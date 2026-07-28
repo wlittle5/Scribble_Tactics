b4c2196e948184b07baaa2668d9f73a4b05bb3e1
wlittle5 <wlornelittle@gmail.com>
2026-07-28 11:24:06 -0500

    Enemy sprite and early logic

---
+++ Scribble_Tactics/Assets/Scripts/EnemyLogic.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using Input = UnityEngine.Input;
+using UnityEngine.Windows;
+using System;
+using UnityEngine.Events;
+
+public class EnemyLogic : MonoBehaviour
+{
+    public event EventHandler OnSelected;
+    public event EventHandler OnDeSelected;
+
+    public static EnemyLogic Instance { get; private set; }
+    
+    [SerializeField] GameObject range;
+
+    private bool isSelected = false;
+    private bool canMove = false;
+    private bool isMoving = false;
+    private int hitData;
+
+    private RaycastHit rayCastHit;
+    private Vector3 mousePos;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    void Update()
+    {
+
+        if (Input.GetMouseButtonUp(0) && isMoving != true)
+        {
+            GetLayer();
+            canMove = MoveCheck();
+        }
+    }
+
+    private void GetLayer()
+    {
+        Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(myRay, out rayCastHit);
+        hitData = rayCastHit.transform.gameObject.layer;
+    }
+
+    private bool MoveCheck()
+    {
+
+        if ((hitData == 6) && !isSelected)
+        {
+            isSelected = true;
+            ShowRange();
+            return false;
+        }
+
+        if ((hitData == 7) && isSelected)
+        {
+            return true;
+        }
+
+        if (((hitData != 7) || (hitData != 6)) && isSelected)
+        {
+            OnDeSelected?.Invoke(this, EventArgs.Empty);
+            isSelected = false;
+            HideRange();
+            return false;
+        }
+
+        else
+        {
+            return isSelected;
+        }
+    }
+
+    private void ShowRange()
+    {
+        range.gameObject.SetActive(true);
+        OnSelected?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void HideRange()
+    {
+        range.gameObject.SetActive(false);
+    }
+    
+    public bool IsSelected()
+    {
+        return isSelected;
+    }
+
+    public bool IsMoving() 
+    {
+        return isMoving;
+    }
+
+}
